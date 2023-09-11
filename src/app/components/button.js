f6b584a6a9381383bacac04ab057f864513a2c3e
VictorHa <victorhabarramansa@gmail.com>
2023-09-11 15:29:22 -0300

    Button text

--- src/app/components/button.js
+++ src/app/components/button.js
@@ -1,20 +1,20 @@
 import React from 'react';
 
-const Button1 = () => {
+const Button1 = ({ texto }) => {
   return (
     <div className="flex flex-col w-full" id="NewRootRoot">
-  <div className="flex items-center justify-center"> {/* Adicionado */}
-    <button
-      className="border-solid shadow-[0px_2px_2px_0px_rgba(0,_0,_0,_0.25)] bg-[#0984e3] flex flex-col items-center pt-0.5 pb-1 border-black border rounded-lg"
-      id="Button1"
-      style={{ width: '100px', height: '30px', overflow: 'hidden' }}
-    >
-      <div className="text-base font-['Noto_Sans'] font-semibold tracking-[1.5] text-white">
-        botão
-      </div>
-    </button>
-  </div> 
-</div>
+      <div className="flex items-center justify-center mt-8">
+        <button
+          className="border-solid shadow-[0px_2px_2px_0px_rgba(0,_0,_0,_0.25)] bg-[#0984e3] flex flex-col items-center pt-0.5 pb-1 border-black border rounded-lg"
+          id="Button1"
+          style={{ width: '100px', height: '30px', overflow: 'hidden' }}
+        >
+          <div className="text-base font-['Noto_Sans'] font-semibold tracking-[1.5] text-white">
+            {texto}
+          </div>
+        </button>
+      </div> 
+    </div>
   );
 };
 
--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,9 @@ export default function Home() {
   return (
     <>
       <Header/>
-      <Button1/>
+      <Button1 width="120px" height="40px" texto="Minhas Bicicletas" />
+      <Button1 width="150px" height="50px" texto="Vistorias" />
+      <Button1 width="100px" height="30px" texto="Fale Conosco FAQ" />
     </>
   )
 }
