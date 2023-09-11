bee39a8abdae57516647bb3520d3c3d71685fb1d
Kelvin Gomes <kelvingomes04@gmail.com>
2023-09-11 15:45:46 -0300

    header 100%

--- src/app/components/header/Header.jsx
+++ src/app/components/header/Header.jsx
@@ -11,9 +11,10 @@ export default function Header(props) {
                />
             </div>
          </div>
-         <div className="w-96 h-20 relative">
-            <div className="w-96 h-20 left-0 top-0 absolute bg-sky-600" />
-            <div className="w-28 left-[154px] top-[16px] absolute text-center text-white text-3xl font-normal tracking-widest">{props.text}</div>
+         <div className="w-full h-20 relative">
+            <div className="w-full h-20 left-0 top-0 absolute bg-sky-600">
+               <div className="flex items-center justify-center h-full text-white text-3xl tracking-widest">{props.text}</div>
+            </div>
          </div>
       </>
    )
--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import Header from "./components/header/Header";
 export default function Home() {
   return (
     <>
-      <Header/>
+      <Header text="Home" />
       <Button1 width="120px" height="40px" texto="Minhas Bicicletas" />
       <Button1 width="150px" height="50px" texto="Vistorias" />
       <Button1 width="100px" height="30px" texto="Fale Conosco FAQ" />
