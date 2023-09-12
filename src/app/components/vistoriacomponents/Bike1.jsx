adc2c3410fe02bc10150c5a83819b43225604afd
luqsampaio <luqsampaiodev@gmail.com>
2023-09-11 23:02:51 -0300

    vistoria components need fix

---
+++ src/app/components/vistoriacomponents/Bike1.jsx
@@ -0,0 +1,8 @@
+export default function Bike1(){
+    return(
+        <div className="w-96 h-28 flex flex-row gap- relative">
+  <img className="w-44 h-50 left-0 top-0 absolute rounded-2xl shadow border border-black" src="https://via.placeholder.com/170x110" />
+  <div className="w-32 h-5 left-[23px] top-[120px] absolute text-black text-base font-normal">Absolute Nero 4</div>
+</div>
+    )
+}
---
+++ src/app/components/vistoriacomponents/Bike2.jsx
@@ -0,0 +1,10 @@
+export default function Bike2(){
+    return(
+        <div className="w-96 h-10 flex-col justify-center items-start inline-flex">
+  <div className="w-96 h-10 relative">
+    <img className="w-44 h-28 left-0 top-20 absolute rounded-2xl shadow border border-black" src="https://via.placeholder.com/170x110" />
+    <div className="w-32 h-5 left-[27px] top-[200px] absolute text-black text-base font-normal">Scott Spark 970</div>
+  </div>
+</div>
+    )
+}
